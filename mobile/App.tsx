9c52cd0987fc0ad1cc5d4a62a479c7cde0e80a95
Akkihiko <joaomanuel.ferreira12@gmail.com>
2022-11-02 17:05:14 -0300

    📦 Aula 03 concluída

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,22 +1,26 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold, Roboto_500Medium } from '@expo-google-fonts/roboto';
 
-import { Loading } from './src/components/Loading';
 import { THEME } from './src/styles/theme';
-import { SignIn } from './src/components/SignIn';
+import { Loading } from './src/components/Loading';
+import { AuthContextProvider } from './src/contexts/AuthContext';
+
+import { Pools } from './src/screens/Pools';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar 
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      
-      { fontsLoaded ? <SignIn /> : <Loading /> }
+      <AuthContextProvider>
+        <StatusBar 
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        
+        { fontsLoaded ? <Pools /> : <Loading /> }
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
